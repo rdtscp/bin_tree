f5da4a8b98f17c64bdacbb3c702c98cdb42d8542
Alexander Wilson <acwilson96@gmail.com>
2018-09-13 23:15:29 +0100

    Test default construction, list initialising, in order traversal, inserting, and clearing.

--- test/Test_BinTree.cpp
+++ test/Test_BinTree.cpp
@@ -10,10 +10,21 @@ using namespace ads;
 
 /* Assert that a Default BinTree Constructs without error. */
 TEST(BinTreeTest, DefaultConstruction) {
-  bin_tree<int> tree({2, 1, 3});
+  bin_tree<int> tree;
+
+	ASSERT_FALSE(tree.find(1));
+}
+
+TEST(BinTreeTest, InitializerListConstructor) {
+	bin_tree<int> tree({2, 1, 3});
+
+	ASSERT_FALSE(tree.find(0));
+	ASSERT_TRUE(tree.find(1));
+	ASSERT_TRUE(tree.find(2));
+	ASSERT_TRUE(tree.find(3));
 
 	std::vector<int> expected 	= { 1, 2, 3 };
-	std::vector<int> actual		 	= tree.in_order();
+	std::vector<int> actual			= tree.in_order();
 
 	bool result = true;
 	for (int i = 0; i < actual.size(); i++) {
@@ -25,6 +36,66 @@ TEST(BinTreeTest, DefaultConstruction) {
 
 	ASSERT_TRUE(result);
 }
+
+TEST(BinTreeTest, VectorConstructor) {
+	bin_tree<int> tree = {2, 1, 3};
+
+	ASSERT_FALSE(tree.find(0));
+	ASSERT_TRUE(tree.find(1));
+	ASSERT_TRUE(tree.find(2));
+	ASSERT_TRUE(tree.find(3));
+
+	std::vector<int> expected 	= { 1, 2, 3 };
+	std::vector<int> actual			= tree.in_order();
+
+	bool result = true;
+	for (int i = 0; i < actual.size(); i++) {
+		if (expected[i] != actual[i]) {
+			result = false;
+			break;
+		}
+	}
+
+	ASSERT_TRUE(result);
+}
+
+TEST(BinTreeTest, InOrderTraversal) {
+	bin_tree<int> tree({10, 5, 2, 3, 4, 1, 8, 9, 7, 6});
+
+	std::vector<int> expected 	= { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	std::vector<int> actual			= tree.in_order();
+
+	ASSERT_EQ(actual, expected);
+}
+
+TEST(BinTreeTest, ClearBinaryTree) {
+	bin_tree<int> tree({2, 1, 3});
+
+	ASSERT_FALSE(tree.find(0));
+	ASSERT_TRUE(tree.find(1));
+	ASSERT_TRUE(tree.find(2));
+	ASSERT_TRUE(tree.find(3));
+
+	tree.clear();
+
+	ASSERT_FALSE(tree.find(0));
+	ASSERT_FALSE(tree.find(1));
+	ASSERT_FALSE(tree.find(2));
+	ASSERT_FALSE(tree.find(3));
+}
+
+TEST(BinTreeTest, InsertElement) {
+	bin_tree<int> tree;
+
+	ASSERT_FALSE(tree.find(0));
+
+	tree.insert(0);
+
+	ASSERT_TRUE(tree.find(1));
+}
+
+// TEST(BinTreeTest)
+
 // The fixture for testing class Project1. From google test primer.
 class Test_BinTree : public ::testing::Test {
 	protected:
