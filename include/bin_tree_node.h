71484f1178928ad8ed14153c00906fa59514d376
Alexander Wilson <acwilson96@gmail.com>
2018-09-13 23:37:18 +0100

    Placeholder for pre_order, implementation for find()

--- include/bin_tree_node.h
+++ include/bin_tree_node.h
@@ -20,49 +20,70 @@ class bin_tree_node {
 
     /* Insert an Element. */
     void insert(const T& element) {
-      printf("\nInserting Element\n");
       if (element < value) {
-        printf("\nGoing Left\n");
         if (left != nullptr)
           left->insert(element);
         else
           left = std::unique_ptr<bin_tree_node<T>>(new bin_tree_node(element));
       }
       else if (element > value) {
-        printf("\nGoing Right\n");
         if (right != nullptr)
           right->insert(element);
         else
           right = std::unique_ptr<bin_tree_node<T>>(new bin_tree_node(element));
       }
       else {
-        printf("\nIM HERE\n");
         value = element;
+      }
+    }
+
+    /* Returns if an Element Exists in the Tree */
+    bool find(const T& element) {
+      if (element < value) {
+        if (left != nullptr)
+          return left->find(element);
+        else
+          return false;
+      }
+      else if (element > value) {
+        if (right != nullptr)
+          return right->find(element);
+        else
+          return false;
+      }
+      else {
+        return value == element;
       }
     }
 
     /* Balance the Binary Tree */
     void balance() {
-      std::cout << "@TODO - Balance Tree" << std::endl;
+      // @TODO
     }
 
     std::vector<T> in_order() {
       std::vector<T> output       = {};
       
-      std::cout << "Getting Left Order" << std::endl;
       if (left != NULL) {
         std::vector<T> leftOrder    = left->in_order();
         output.insert(output.end(), leftOrder.begin(), leftOrder.end());
       }
 
-      std::cout << "Getting This Element" << std::endl;
       output.push_back(value);
       
-      std::cout << "Getting Right Order" << std::endl;
       if (right != NULL) {
         std::vector<T> rightOrder   = right->in_order();
         output.insert(output.end(), rightOrder.begin(), rightOrder.end());
       }
+
+      return output;
+    }
+
+    std::vector<T> pre_order() {
+      std::vector<T> output;
+
+      /* Temporary */
+      return in_order();
 
       return output;
     }
