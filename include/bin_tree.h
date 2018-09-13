6ba7f69c536e189123dbcbe0289e1951c9826487
Alexander Wilson <acwilson96@gmail.com>
2018-09-13 22:21:33 +0100

    Basic Unfinished BinTree implementation.

--- include/bin_tree.h
+++ include/bin_tree.h
@@ -3,20 +3,59 @@
 
 #include <memory>
 #include <vector>
+#include <functional>
 
 #include "bin_tree_node.h"
 
 
 namespace ads {
-  
-  class bin_tree {
 
+  
+  template <typename T>
+  class bin_tree {
+    
     public:
 
       /* Default Constructor */
-      bin_tree();
+      bin_tree<T>() {}
+
+      /* Takes in a vector of elements, and constructs a binary tree. */
+      bin_tree<T>(std::vector<T> elements) {
+        for (const T& element: elements) {
+          insert(element);
+        }
+        balance();
+      }
+
+      /* Initialiser-List Constructor */
+      bin_tree<T>(std::initializer_list<T> elements) {
+        for (const T& element: elements) {
+          insert(element);
+        }
+        balance();
+      }
+
+      /* Insert an Element. */
+      void insert(const T& element) {
+        if (root == nullptr)
+          root = std::unique_ptr<bin_tree_node<T>>(new bin_tree_node<T>(element));
+        root->insert(element);
+      }
+
+      /* Balance the Binary Tree */
+      void balance() {
+        root->balance();
+      }
+
+      std::vector<T> in_order() {
+        std::vector<T> result = root->in_order();
+        return result;
+      }
 
     private:
+
+      int height = 0;
+      std::unique_ptr<bin_tree_node<T>> root;
 
   };
 
--- include/bin_tree_node.h
+++ include/bin_tree_node.h
@@ -3,18 +3,76 @@
 
 #include <memory>
 #include <vector>
+#include <iostream>
 
 #include "bin_tree.h"
-  
+
+template <typename T>
 class bin_tree_node {
 
   public:
 
     /* Default Constructor */
-    bin_tree_node();
+    bin_tree_node<T>() {}
+
+    /* Construct with a Value */
+    bin_tree_node<T>(const T& element) : value(element) {}
+
+    /* Insert an Element. */
+    void insert(const T& element) {
+      printf("\nInserting Element\n");
+      if (element < value) {
+        printf("\nGoing Left\n");
+        if (left != nullptr)
+          left->insert(element);
+        else
+          left = std::unique_ptr<bin_tree_node<T>>(new bin_tree_node(element));
+      }
+      else if (element > value) {
+        printf("\nGoing Right\n");
+        if (right != nullptr)
+          right->insert(element);
+        else
+          right = std::unique_ptr<bin_tree_node<T>>(new bin_tree_node(element));
+      }
+      else {
+        printf("\nIM HERE\n");
+        value = element;
+      }
+    }
+
+    /* Balance the Binary Tree */
+    void balance() {
+      std::cout << "@TODO - Balance Tree" << std::endl;
+    }
+
+    std::vector<T> in_order() {
+      std::vector<T> output       = {};
+      
+      std::cout << "Getting Left Order" << std::endl;
+      if (left != NULL) {
+        std::vector<T> leftOrder    = left->in_order();
+        output.insert(output.end(), leftOrder.begin(), leftOrder.end());
+      }
+
+      std::cout << "Getting This Element" << std::endl;
+      output.push_back(value);
+      
+      std::cout << "Getting Right Order" << std::endl;
+      if (right != NULL) {
+        std::vector<T> rightOrder   = right->in_order();
+        output.insert(output.end(), rightOrder.begin(), rightOrder.end());
+      }
+
+      return output;
+    }
 
   private:
 
-  };
+    T value;
+    std::unique_ptr<bin_tree_node<T>> left;
+    std::unique_ptr<bin_tree_node<T>> right;
+
+};
 
 #endif
